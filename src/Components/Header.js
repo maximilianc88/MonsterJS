e5e27cfc4b502be3892aa244875c8f5858caa3b2
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-05-02 15:27:14 -0700

    added header file and div

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div>
+      <div className="comp-cat headers">
+        <h1> Headers:</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
