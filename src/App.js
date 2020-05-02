54bd2198bb1347e808055fecc74261034a9f9c74
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-05-02 15:29:44 -0700

    added footers div

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = (props) => {
+  return (
+    <div>
+      <div className="comp-cat footers">
+        <h1> Footers:</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
