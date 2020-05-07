6039e30aeb702018738ecbc28b91809f1b990169
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-05-06 19:29:11 -0700

    update css on button to match header

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './style.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.css";
 
-const Button = ({styleClass, value}) => (
-    <button
-        className = {styleClass} >
-        {value} 
-        </button>
+const Button = ({ styleClass, value }) => (
+  <button className={styleClass}>{value}</button>
 );
 
 Button.propTypes = {
-    styleClass: PropTypes.string,
-    value: PropTypes.string,
+  styleClass: PropTypes.string,
+  value: PropTypes.string,
 };
 
 Button.defaultProps = {
-    styleClass: 'btn-default'
+  styleClass: "btn-default",
 };
 
 export default Button;
