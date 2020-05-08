0eb744c3134541eb7060c9f9979561961794f98e
Marie Lenac <lenacm02@gmail.com>
2020-05-08 16:20:06 -0700

    little stuff working on font awesome

--- src/Components/Figure/index.jsx
+++ src/Components/Figure/index.jsx
@@ -1,16 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Figure = ({theme, value, image}) => (
-    
-    <div className ={`${ theme }`}>
-        <img src={`${ image }`}>
-    
-        </img>
+const Figure = ({ theme, value, image }) => (
+  <div className={`${theme}`}>
+    <img alt="fig-img" src={`${image}`}></img>
 
-        <p>{value}</p>
-
-    </div>
-
-)
+    <p>{value}</p>
+  </div>
+);
 
 export default Figure;
