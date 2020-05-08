daa3cbec6e5dec66dec2f6114ce5ab5ea1e84b31
Marie Lenac <lenacm02@gmail.com>
2020-05-08 16:32:37 -0700

    search bar is styled in stories

--- src/Components/SearchBar/index.jsx
+++ src/Components/SearchBar/index.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const SearchBar = ({theme, value}) => (
-    
-    <div className={` ${ theme }`}>
+const SearchBar = ({ theme, value, placeholderText }) => {
+  return (
+    <div className={`search-bar col-lg-6 col-sm-12 ${theme}`}>
+      <input
+        className="search-input"
+        type="text"
+        placeholder={placeholderText}
+      ></input>
 
-        <input type="text"></input>
-
-        <button>{ value }</button>
+      <button className={theme}>{value}</button>
     </div>
-
-)
-
+  );
+};
 export default SearchBar;
