4477c7227a9418529c6cbda28774c4e0ca17a56b
Randall Burgess <randallallenburgess@gmail.com>
2020-05-08 09:01:45 -0700

    everything component has their own story loaded up.  Issues adding some components, and css not always rendering (#30)

---
+++ src/stories/8-Social.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '@storybook/react/demo';
+import Card from "../Components/Card";
+import Social from "../Components/Social";
+
+export default {
+  title: 'Social',
+  component: Social,
+};
+
+export const All = () => 
+<Social />
