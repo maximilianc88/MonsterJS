621608e163887d598d6fc64c0cef4a210a9bddf8
Marie Lenac <lenacm02@gmail.com>
2020-05-08 14:37:39 -0700

    installed storybook and fixed the card file

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,23 +1,27 @@
-
 import React from "react";
 import Proptypes from "prop-types";
 import "./style.css";
 
-
 const Card = ({ theme, cardImage, value, title }) => {
   return (
     <wrapper>
-    <div>
-      <div className="row">
-        <div className="col-md-4">
-          <div className={`card ${theme}`}>
-            <div><img className="img-fluid" src={cardImage} alt="Gaga"  /></div>
-                <div><span className="card">{title}</span></div>
-            <div><p className="card-p">{value}</p></div>
+      <div>
+        <div className="row">
+          <div className="col-md-4">
+            <div className={`card ${theme}`}>
+              <div>
+                <img className="img-fluid" src={cardImage} alt="Gaga" />
+              </div>
+              <div>
+                <span className="card">{title}</span>
+              </div>
+              <div>
+                <p className="card-p">{value}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </wrapper>
   );
 };
@@ -25,11 +29,6 @@ const Card = ({ theme, cardImage, value, title }) => {
 Card.propTypes = {
   theme: Proptypes.string,
   cardImage: Proptypes.string,
-<<<<<<< HEAD
-=======
-  imageHeight: Proptypes.string,
-  imageWidth: Proptypes.string,
->>>>>>> 62c7933d649a1eeb2f1eb4434a7cd20d629ac3c7
   value: Proptypes.string,
   title: Proptypes.string,
 };
